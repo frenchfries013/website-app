81a48a26298f7a73eadc5af36ed6fde618948ad3
Riley North <rileynorth4@gmail.com>
2020-03-09 21:09:18 -0500

    Updated toolbar

--- my-app/src/components/toolbar.jsx
+++ my-app/src/components/toolbar.jsx
@@ -5,33 +5,37 @@ import "../index.css";
 class Toolbar extends Component {
   render() {
     return (
-      <div class="top-toolbar">
-        <span class="col-2 float-left">
-          <a class="col-2" href="#home">
-            Home
-          </a>
-          <a class="col-2" href="#about">
-            About
-          </a>
-          <a class="col-2" href="#contact">
-            Contact
-          </a>
-        </span>
-        <span class="col-2 float-right">
-          <form action="">
-            <input
-              class="m-2 col-6"
-              type="text"
-              placeholder="Search.."
-              name="search"
-            />
-            <button type="submit">
-              <i class="">
-                <IoMdSearch />
-              </i>
-            </button>
-          </form>
-        </span>
+      <div class="bg-primary text-secondary">
+        <nav class="navbar navbar-dark bg-dark">
+          <span class="col-2 float-left">
+            <a class="col-2 text-secondary" href="#home">
+              Home
+            </a>
+            <a class="col-2 text-secondary" href="#about">
+              About
+            </a>
+            <a class="col-2 text-secondary" href="#contact">
+              Contact
+            </a>
+          </span>
+          <h2 class=".col-md-4 text-center">Website App!</h2>
+          <span>
+            <form class="form-inline my-2 my-lg-0">
+              <input
+                class="form-control mr-sm-2"
+                type="text"
+                placeholder="Search.."
+                name="search"
+              />
+              <button
+                class="btn btn-outline-secondary my-2 my-sm-0"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+          </span>
+        </nav>
       </div>
     );
   }
